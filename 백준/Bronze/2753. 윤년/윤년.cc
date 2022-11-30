9fd8215ad00220c51f59aa62209b591ebb6c3516
YooJeong <jeongex@gmail.com>
2022-11-30 23:14:44 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,21 @@
+using namespace std;
+#include <iostream>
+
+int main() {
+
+	int year;
+	cin >> year;
+
+	if (year % 4 == 0) {
+		if (year % 100 != 0 || year % 400 ==0 ) {
+			cout << 1;
+		}
+		else {
+			cout << 0;
+		}
+	}
+	else {
+		cout << 0;
+	}
+
+}
