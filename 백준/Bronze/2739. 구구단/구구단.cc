b0f2fc47a9186a7d7dab86c1050dc5a604fed7c4
YooJeong <jeongex@gmail.com>
2022-12-01 21:23:57 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 1; i < 10; i++) {
+
+		cout << N << " * " << i << " = " << N * i << "\n";
+
+	}
+
+}
