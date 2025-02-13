52c3fa568c8039accae647bfdcc8c849e91e81fa
YooJeong <jeongex@gmail.com>
2025-02-13 19:40:19 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -7,14 +8,8 @@ double solution(vector<int> arr) {
     double answer = 0;
     
     // 1.arr의 모든 원소를 순회하며 더한다.
-    int iCount = arr.size();
-    for(int i = 0; i < iCount; ++i)
-        {
-            answer += arr[i];
-        }
+    answer = accumulate(arr.begin(), arr.end(), 0);
     
     // 2.arr의 원소 갯수만큼 나눈다.
-    answer = answer / iCount;
-
-    return answer;
+    return answer / arr.size();
 }
